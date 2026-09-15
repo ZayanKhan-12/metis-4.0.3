ZayanKhan-12/metis-4.0.3

commit fb42c7c8a47c27b9f7def4e4b54b5df8b8b620ef

adding partnmesh verbose option

Programs/partnmesh.c
@@ -3,7 +3,7 @@
  *
  * partnmesh.c
  *
- * This file reads in the element node connectivity array of a mesh and 
+ * This file reads in the element node connectivity array of a mesh and
  * partitions both the elements and the nodes using KMETIS on the dual graph.
  *
  * Started 9/29/97
@@ -18,8 +18,8 @@
 
 
 /*************************************************************************
-* Let the game begin
-**************************************************************************/
+ * Let the game begin
+ **************************************************************************/
 main(int argc, char *argv[])
 {
   int i, j, ne, nn, etype, numflag=0, nparts, edgecut;
@@ -28,9 +28,19 @@ main(int argc, char *argv[])
   char etypestr[4][5] = {"TRI", "TET", "HEX", "QUAD"};
   GraphType graph;
 
-  if (argc != 3) {
-    printf("Usage: %s <meshfile> <nparts>\n",argv[0]);
+  if (argc < 3 || argc > 4) {
+    printf("Usage: %s <meshfile> <nparts> [-v]\n",argv[0]);
+    printf("        -v              Show verbose information\n");
     exit(0);
+  }
+  bool verbose = false;
+  if (argc == 4) {
+    if (strcmp("-v",argv[3]) != 0) {
+      printf("Usage: %s <meshfile> <nparts> [-v]\n",argv[0]);
+      printf("        -v              Show verbose information\n");
+      exit(0);
+    }
+    verbose = true;
   }
 
   nparts = atoi(argv[2]);
@@ -38,7 +48,7 @@ main(int argc, char *argv[])
     printf("nparts must be greater than one.\n");
     exit(0);
   }
-   
+
   cleartimer(IOTmr);
   cleartimer(DUALTmr);
 
@@ -48,43 +58,32 @@ main(int argc, char *argv[])
 
   epart = idxmalloc(ne, "main: epart");
   npart = idxmalloc(nn, "main: npart");
-
-  printf("**********************************************************************\n");
-  printf("%s", METISTITLE);
-  printf("Mesh Information ----------------------------------------------------\n");
-  printf("  Name: %s, #Elements: %d, #Nodes: %d, Etype: %s\n\n", argv[1], ne, nn, etypestr[etype-1]);
-  printf("Partitioning Nodal Graph... -----------------------------------------\n");
-
+  if (verbose) {
+    printf("**********************************************************************\n");
+    printf("%s", METISTITLE);
+    printf("Mesh Information ----------------------------------------------------\n");
+    printf("  Name: %s, #Elements: %d, #Nodes: %d, Etype: %s\n\n", argv[1], ne, nn, etypestr[etype-1]);
+    printf("Partitioning Nodal Graph... -----------------------------------------\n");
+  }
 
   starttimer(DUALTmr);
   METIS_PartMeshNodal(&ne, &nn, elmnts, &etype, &numflag, &nparts, &edgecut, epart, npart);
   stoptimer(DUALTmr);
 
-  printf("  %d-way Edge-Cut: %7d, Balance: %5.2f\n", nparts, edgecut, ComputeElementBalance(ne, nparts, epart));
+  if( verbose)
+    printf("  %d-way Edge-Cut: %7d, Balance: %5.2f\n", nparts, edgecut, ComputeElementBalance(ne, nparts, epart));
 
   starttimer(IOTmr);
   WriteMeshPartition(argv[1], nparts, ne, epart, nn, npart);
   stoptimer(IOTmr);
 
 
-  printf("\nTiming Information --------------------------------------------------\n");
-  printf("  I/O:          \t\t %7.3f\n", gettimer(IOTmr));
-  printf("  Partitioning: \t\t %7.3f\n", gettimer(DUALTmr));
-  printf("**********************************************************************\n");
-
-/*
-  graph.nvtxs = ne;
-  graph.xadj = idxmalloc(ne+1, "xadj");
-  graph.vwgt = idxsmalloc(ne, 1, "vwgt");
-  graph.adjncy = idxmalloc(10*ne, "adjncy");
-  graph.adjwgt = idxsmalloc(10*ne, 1, "adjncy");
-
-  METIS_MeshToDual(&ne, &nn, elmnts, &etype, &numflag, graph.xadj, graph.adjncy);
-
-  ComputePartitionInfo(&graph, nparts, epart);
-
-  GKfree(&graph.xadj, &graph.adjncy, &graph.vwgt, &graph.adjwgt, LTERM);
-*/
+  if( verbose) {
+    printf("\nTiming Information --------------------------------------------------\n");
+    printf("  I/O:          \t\t %7.3f\n", gettimer(IOTmr));
+    printf("  Partitioning: \t\t %7.3f\n", gettimer(DUALTmr));
+    printf("**********************************************************************\n");
+  }
 
   GKfree(&elmnts, &epart, &npart, LTERM);
 

commit f658b5f8b668a6eaa2ac31006f637ba771731999

fix: make Programs/ and Test/ compile under C99
Two pre-existing defects that a C89 compiler tolerated:

  * All nine programs plus Test/mtest.c declared main() with no return
    type. Implicit int was removed in C99 and is an error by default in
    GCC 14 and Clang 16.

  * Programs/partnmesh.c used bool/true/false without including
    <stdbool.h>, and declared the variable mid-block. This has been
    broken since fb42c7c ("adding partnmesh verbose option"); the later
    cleanups c7b3350 ("bool with int") and 22a4c66 ("c89 requires vars
    declared at top of function") fixed the same pattern elsewhere but
    missed this file, so partnmesh.c has never compiled since.

partnmesh.c is fixed the way this repository already fixed the others:
an int flag declared at the top of the function. This also matches
partnmesh() in Lib/io.c, which already takes an int verbose parameter.

Co-Authored-By: Claude Opus 5 (1M context) <noreply@anthropic.com>

Programs/graphchk.c
@@ -19,7 +19,7 @@
 /*************************************************************************
 * Let the game begin
 **************************************************************************/
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
   GraphType graph;
   char filename[256];

Programs/kmetis.c
@@ -19,7 +19,7 @@
 /*************************************************************************
 * Let the game begin
 **************************************************************************/
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
   int i, nparts, options[10];
   idxtype *part;

Programs/mesh2dual.c
@@ -20,7 +20,7 @@
 /*************************************************************************
 * Let the game begin
 **************************************************************************/
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
   int i, j, ne, nn, etype, numflag=0;
   idxtype *elmnts, *xadj, *adjncy;

Programs/mesh2nodal.c
@@ -20,7 +20,7 @@
 /*************************************************************************
 * Let the game begin
 **************************************************************************/
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
   int i, j, ne, nn, etype, numflag=0;
   idxtype *elmnts, *xadj, *adjncy;

Programs/oemetis.c
@@ -19,7 +19,7 @@
 /*************************************************************************
 * Let the game begin
 **************************************************************************/
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
   int i, options[10];
   idxtype *perm, *iperm;

Programs/onmetis.c
@@ -19,7 +19,7 @@
 /*************************************************************************
 * Let the game begin
 **************************************************************************/
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
   int i, options[10];
   idxtype *perm, *iperm;

Programs/partdmesh.c
@@ -20,7 +20,7 @@
 /*************************************************************************
 * Let the game begin
 **************************************************************************/
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
   int i, j, ne, nn, etype, numflag=0, nparts, edgecut;
   idxtype *elmnts, *epart, *npart;

Programs/partnmesh.c
@@ -20,9 +20,10 @@
 /*************************************************************************
  * Let the game begin
  **************************************************************************/
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
   int i, j, ne, nn, etype, numflag=0, nparts, edgecut;
+  int verbose = 0;
   idxtype *elmnts, *epart, *npart;
   timer IOTmr, DUALTmr;
   char etypestr[4][5] = {"TRI", "TET", "HEX", "QUAD"};
@@ -33,14 +34,13 @@ main(int argc, char *argv[])
     printf("        -v              Show verbose information\n");
     exit(0);
   }
-  bool verbose = false;
   if (argc == 4) {
     if (strcmp("-v",argv[3]) != 0) {
       printf("Usage: %s <meshfile> <nparts> [-v]\n",argv[0]);
       printf("        -v              Show verbose information\n");
       exit(0);
     }
-    verbose = true;
+    verbose = 1;
   }
 
   nparts = atoi(argv[2]);

Programs/pmetis.c
@@ -19,7 +19,7 @@
 /*************************************************************************
 * Let the game begin
 **************************************************************************/
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
   int i, nparts, options[10];
   idxtype *part;

Test/mtest.c
@@ -20,7 +20,7 @@
 /*************************************************************************
 * Let the game begin
 **************************************************************************/
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
   int i, nparts, options[10];
   idxtype *part;